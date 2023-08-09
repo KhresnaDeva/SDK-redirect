7d6cdc821865a187cf757c0a30395d21696020f0
HaniifAJ <haniif02aj@gmail.com>
2023-08-09 10:39:43 +0700

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+function formEncode (payload) {
+    var obj = Object.entries(payload)
+    .map(([key, value]) => {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    })
+    .join("&");
+    console.log("OBJ ", obj);
+    return obj;
+}
+
+exports.iam = {
+    iam_server_url: 'https://api-oss.domain-dev.site/',
+    client_id: '',
+    redirect_uri: '',
+    audience: '',
+    scope: '',
+    redirect(){
+        let payload = {
+            client_id: this.client_id,
+            redirect_uri: this.redirect_uri,
+            audience: this.audience,
+            scope: this.scope
+        }
+        let redirect_url = this.iam_server_url + 'authorize?' + formEncode(payload)
+        return redirect_url
+    }
+}
